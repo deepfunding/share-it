c64719596a81efd7c5860ba07534a885c5bfc393
Javier Ríos <javier.rios-urbano@epitech.eu>
2025-01-20 23:51:04 +0100

    add: new `graph` layout (#2)
    * fix: add `new` visualization image
    
    * add: new `simulation` parameters for cosmograph
    
    * add: new `graph` link system with `root` node

--- islands/Graph.tsx
+++ islands/Graph.tsx
@@ -1,9 +1,27 @@
-import { Cosmograph } from "@cosmograph/react";
-import { type GraphData } from "~/islands/Home.tsx";
+import { useEffect, useRef } from "preact/hooks";
+import { Cosmograph, type CosmographRef } from "@cosmograph/react";
+import {
+  type GraphData,
+  type Link,
+  type Node,
+  ROOT_NODE,
+  ROOT_NODE_COLOR,
+} from "~/islands/Home.tsx";
 
-const Graph: React.FC<{ data: GraphData; zoom: number }> = ({ data, zoom }) => {
+const Graph: React.FC<{ data: GraphData; zoom: number }> = ({ data }) => {
+  const ref = useRef<CosmographRef<Node, Link>>(null);
+
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    ref.current.zoomToNode(data.nodes.find(({ id }) => id === ROOT_NODE)!);
+  }, [ref]);
+
   return (
     <Cosmograph
+      ref={ref}
       nodes={data.nodes}
       links={data.links}
       nodeColor={(node) => {
@@ -11,19 +29,27 @@ const Graph: React.FC<{ data: GraphData; zoom: number }> = ({ data, zoom }) => {
         const r = Math.round(100 + (70 * value));
         const g = Math.round(149 - (100 * value));
         const b = Math.round(237 - (100 * value));
-        return `rgb(${r}, ${g}, ${b})`;
+        return node.color ?? `rgb(${r}, ${g}, ${b})`;
       }}
       nodeLabelAccessor={(node) => node.label}
       nodeLabelColor={`rgb(255, 255, 255)`}
       hoveredNodeLabelColor={`rgb(255, 255, 255)`}
       nodeSize={(node) => node.size}
-      linkWidth={(link) => link.value}
-      linkColor={() => `rgb(128, 128, 128)`}
-      initialZoomLevel={zoom}
-      nodeSizeScale={1 + zoom / 10}
+      linkWidth={(link) => link.weight}
+      linkColor={`rgba(${ROOT_NODE_COLOR.join(", ")}, 0.5)`}
       showDynamicLabels={false}
-      useQuadtree={true}
+      useQuadtree
       backgroundColor="rgb(255, 255, 255)"
+      spaceSize={2048}
+      fitViewOnInit
+      simulationRepulsion={0.1}
+      simulationRepulsionTheta={0.1}
+      simulationCenter={0.5}
+      linkArrows={false}
+      simulationGravity={0.01}
+      curvedLinks
+      simulationLinkDistance={3}
+      simulationLinkDistRandomVariationRange={[0.8, 1.5]}
     />
   );
 };
